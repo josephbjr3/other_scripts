e662039ed90444bebba960087f5b836fd3dbeba6
josephbjr3 <josephbrinkleyjr21@gmail.com>
2023-03-06 11:50:52 -0500

    ...

--- productivity_hacks/rm_spaces_in_folder_names.zsh
+++ productivity_hacks/rm_spaces_in_folder_names.zsh
@@ -1,9 +1,31 @@
 #!/bin/zsh
 
-# Replace spaces with underscores in file and folder names that are under the home directory "~/"
+# This scripts renames files and folders in the "~/Documents" directory, 
+# replacing spaces with underscores for easier navigation in the CLI. 
+# I'm still working on it. Right now, it only works one level down from the "~/Documents" directory
 
-while true; do
-  find ~ -depth -name "* *.*" -type f -execdir rename 's/ /_/g' "{}" \;
-  find ~ -depth -name "* *.*" -type d -execdir rename 's/ /_/g' "{}" \;
-  sleep 60
+echo "Renaming files and folders in ~/Documents directory..."
+
+# Loop through all files and folders in the ~/Documents directory
+for file in ~/Documents/*; do
+    if [[ -d "$file" ]]; then
+        # If it's a directory, check if it contains spaces in the name
+        if echo "$file" | grep -q " "; then
+            # If it contains spaces, replace them with underscores
+            new_name="$(echo "$file" | sed 's/ /_/g')"
+            echo "Renaming directory: $file -> $new_name"
+            mv -v "$file" "$new_name"
+        fi
+    elif [[ -f "$file" ]]; then
+        # If it's a file, check if it contains spaces in the name
+        if echo "$file" | grep -q " "; then
+            # If it contains spaces, replace them with underscores
+            new_name="$(echo "$file" | sed 's/ /_/g')"
+            echo "Renaming file: $file -> $new_name"
+            mv -v "$file" "$new_name"
+        fi
+    fi
 done
+
+echo "Done!"
+
